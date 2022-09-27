f5ef9514bf071fbd6a61ade782d82bb4bf6e16e8
Alejandro Ruiz <ale.gr92@yahoo.com.ar>
2022-09-27 14:18:51 -0300

    Agregando librerias

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
+//CODIGO DE LA LIBRERIA SWIPER JS,GENERA UN SLIDER
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  autoplay: {
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+
+  breakpoints: {
+    500: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    1020: {
+      slidesPerView: 3,
+      spaceBetween: 50,
+    },
+    1500: {
+      slidesPerView: 4,
+      spaceBetween: 60,
+    },
+  },
+});
+
 let juegos = [
   {
     id: 0,
@@ -246,6 +274,7 @@ let juegos = [
 let total = 0;
 let contador = 0;
 let carrito = [];
+//LAS VARIABLES QUE LLEVAN $ ADELANTE APUNTAN AL DOM
 let $productsSwitch = document.querySelector(".products-switch");
 let $productsPs5 = document.querySelector(".products-ps5");
 let $templateCard = document.getElementById("template-card").content;
@@ -263,6 +292,8 @@ let $modalWindowCart = document.querySelector(".modal-cart");
 
 // DECLARACION DE FUNCIONES////////////////////////////////////////////////////////////////////
 
+//PINTA LOS PRODUCTOS EN EL HTML
+//@param { juegos } es el array donde estan todos los productos
 let injectProducts = (juegos) => {
   for (let juego of juegos) {
     //USANDO DESTRUCTURACION
@@ -295,6 +326,9 @@ let injectProducts = (juegos) => {
   $productsPs5.appendChild($fragmentPs5);
 };
 
+// FUNCION PRINCIPAL PARA AGREGAR UN PRODUCTO AL CARRITO
+// @param { obj } es un nodo con los elementos de la card del producto
+// @param { carrito } es el array donde se van pusheando los productos
 let setCarrito = (obj, carrito) => {
   let producto = {
     id: obj.querySelector("button").getAttribute("id"),
@@ -322,6 +356,7 @@ let setCarrito = (obj, carrito) => {
   injectTotal();
 };
 
+//PINTA EL TOTAL EN LA VENTANA DEL CARRITO
 let injectTotal = () => {
   total = 0;
   let div = document.createElement("div");
@@ -343,6 +378,8 @@ let injectTotal = () => {
   $carritoWindow.insertAdjacentElement("beforeend", div);
 };
 
+//REFRESCA EL TOTAL EN LA VENTANA DEL CARRITO
+//esta funcion se usara cuando se le sume o reste cantidades al producto o cuando se elimine
 let refreshTotal = () => {
   let $parrafoTotal = document.querySelector(".total-p");
   total = 0;
@@ -352,6 +389,7 @@ let refreshTotal = () => {
   $parrafoTotal.textContent = `Total a pagar: ${total} `;
 };
 
+//PINTA EN LA VENTANA DEL CARRITO, LOS PRODUCTOS QUE VALLAN AGREGANDO
 let injectCarrito = () => {
   carrito.forEach((producto) => {
     //USANDO DESTRUCTURACION
@@ -375,6 +413,9 @@ let injectCarrito = () => {
   $carritoWindow.appendChild($fragmentCarrito);
 };
 
+//AUMENTA EL CONTADOR SI EL PRODUCTO NO EXISTE EN EL CARRITO
+// @param { obj } es un nodo con los elementos de la card del producto para poder sacar el id del boton
+// @param { carrito } es el array donde se van pusheando los productos
 let addCounter = (obj, carrito) => {
   let id = obj.querySelector("button").getAttribute("id");
   let existe = carrito.some((obj) => obj.id === id);
@@ -382,6 +423,8 @@ let addCounter = (obj, carrito) => {
   !existe && contador++;
 };
 
+//MUESTRA EL DIV CONTADOR (ELIMINANDO LA PROPIEDAD DISPLAY-NONE) DONDE SE VA A IR MOSTRANDO LA CANTIDAD DE PRODUCTOS
+//@param { cont } es el contador
 let injectCounter = (cont) => {
   if (cont !== 0) {
     $divContador.classList.add("active");
@@ -391,6 +434,7 @@ let injectCounter = (cont) => {
   }
 };
 
+//MUESTRA LA VENTANA MODAL (ELIMINANDO LA PROPIEDAD DISPLAY-NONE)  QUE SE ABRIRA CUANDO HAGAN CLICK EN EL BOTON AGREGAR CARRITO
 let showModal = (obj) => {
   let img = obj.querySelector(".sec-img").getAttribute("src");
   let title = obj.querySelector(".title-card").textContent;
@@ -406,6 +450,7 @@ let showModal = (obj) => {
   }, 2000);
 };
 
+//MUESTRA LA VENTANA MODAL (ELIMINANDO LA PROPIEDAD DISPLAY-NONE) QUE SURGIRA CUANDO ACTUALIZEMOS LA PAGINA Y TENGAMOS PRODUCTOS DEL CARRITO GUARDADOS EN LOCALSTORAGE
 let showModalCart = () => {
   let card = $modalWindowCart.querySelector(".modal");
   $modalWindowCart.classList.remove("visible");
@@ -415,6 +460,11 @@ let showModalCart = () => {
   }, 2000);
 };
 
+//ELIMINA UN PRODUCTO DEL CARRITO Y DISMINUYE EN 1 EL CONTADOR
+//SI EL CONTADOR LLEGA A 0 PINTA UN MENSAJE QUE DICE "CARRITO VACIO"
+// @param { obj } es un nodo con los elementos de la card del producto para poder sacar el id del boton
+// @param { carrito } es el array donde se van pusheando los productos
+// @param { ventana } es una variable que apunta al DOM que con el metodo removeChild() se eliminara el div del producto seleccionado
 let removeProduct = (obj, carrito, ventana) => {
   let id = obj.querySelector(".id").textContent;
   for (let index = 0; index < carrito.length; index++) {
@@ -435,6 +485,9 @@ let removeProduct = (obj, carrito, ventana) => {
   }
 };
 
+//INCREMENTA EN 1 LA CANTIDAD DEL PRODUCTO SELECCIONADO,CALCULA EL SUBTOTAL Y LO VA ACTUALIZANDO EN EL HTML
+// @param { obj } es un nodo con los elementos de la card del producto para poder sacar el id del boton
+// @param { carrito } es el array donde se van pusheando los productos
 let increaseProduct = (obj, carrito) => {
   let id = obj.querySelector(".id").textContent;
   let cantidad = obj.querySelector(".mid-p");
@@ -449,6 +502,10 @@ let increaseProduct = (obj, carrito) => {
   });
 };
 
+//DISMINUYE EN 1 LA CANTIDAD DEL PRODUCTO SELECCIONADO,CALCULA EL SUBTOTAL Y LO VA ACTUALIZANDO EN EL HTML
+//LA CANTIDAD DE PRODUCTOS NO PUEDE DISMINUIR DE 1
+// @param { obj } es un nodo con los elementos de la card del producto para poder sacar el id del boton
+// @param { carrito } es el array donde se van pusheando los productos
 let decreaseProduct = (obj, carrito) => {
   let id = obj.querySelector(".id").textContent;
   let cantidad = obj.querySelector(".mid-p");
@@ -465,19 +522,42 @@ let decreaseProduct = (obj, carrito) => {
   });
 };
 
+//GUARDA EL CARRITO EN EL LOCALSTORAGE
 let storageCarrito = () => {
   let json = JSON.stringify(carrito);
   localStorage.setItem("carrito", json);
 };
 
+//GUARDA EL CONTADOR EN EL LOCALSTORAGE
 let storageCounter = () => {
   let json = JSON.stringify(contador);
   localStorage.setItem("contador", json);
 };
 
+//FUNCIONA DE LA LIBRERIA TOASTIFY PARA MOSTRAR UNA NOTIFICACION CUANDO SE ELIMINE UN PRODUCTO
+let toastify = () => {
+  Toastify({
+    text: "Producto eliminado correctamente",
+    duration: 3000,
+    destination: "https://github.com/apvarun/toastify-js",
+    newWindow: true,
+    close: true,
+    gravity: "bottom",
+    position: "right",
+    stopOnFocus: true,
+    style: {
+      background:
+        "linear-gradient(90deg, rgba(241,12,36,1) 0%, rgba(252,48,12,1) 100%)",
+    },
+  }).showToast();
+};
+
 // EJECUCION DEL SCRIPT//////////////////////////////////////////////////////////////////////////////////
 
 document.addEventListener("DOMContentLoaded", (e) => {
+  //EN EL EVENTO DE CARGA DEL DOM PINTA LOS PRODUCTOS Y PREGUNTA SI CARRITO EXISTE EN LOCALSTORAGE
+  //SI EXISTE CARRITO TOMA ESOS VALORES GUARDADOS Y SE MUESTRAN EN LA VENTANA DEL CARRITO
+
   injectProducts(juegos);
   let carritoStorage = localStorage.getItem("carrito");
   let contadorStorage = localStorage.getItem("contador");
@@ -494,6 +574,7 @@ document.addEventListener("DOMContentLoaded", (e) => {
 });
 
 document.addEventListener("click", (e) => {
+  //EVENTO QUE SUCEDERA SI HACEN CLICK EN EL BOTON DE AGREGAR PRODUCTO AL CARRITO DE CUALQUIER CARD
   if (e.target.matches("button")) {
     showModal(e.target.parentElement);
     addCounter(e.target.parentElement, carrito);
@@ -504,26 +585,31 @@ document.addEventListener("click", (e) => {
     storageCounter();
   }
 
+  //EVENTO QUE SUCEDERA SI HACEN CLICK EN EL CARRITO
   if (e.target.matches(".carrito")) {
     $divCarrito.classList.toggle("transform");
   }
 
+  //EVENTO QUE SUCEDERA SI HACEN CLICK EN LA FLECHITA APUNTANDO HACIA ARRIBA QUE AUMENTA LA CANTIDAD DE UN PRODUCTO
   if (e.target.matches(".up")) {
     increaseProduct(e.target.parentNode.parentElement, carrito);
     refreshTotal();
     storageCarrito();
   }
 
+  //EVENTO QUE SUCEDERA SI HACEN CLICK EN LA FLECHITA APUNTANDO HACIA ABAJO QUE DISMINUYE LA CANTIDAD DE UN PRODUCTO
   if (e.target.matches(".down")) {
     decreaseProduct(e.target.parentNode.parentElement, carrito);
     refreshTotal();
     storageCarrito();
   }
 
+  //EVENTO QUE SUCEDERA SI HACEN CLICK EN LA PAPELERA QUE PERMITE ELIMINAR UN PRODUCTO
   if (e.target.matches(".delete")) {
     removeProduct(e.target.parentElement, carrito, $carritoWindow);
     // USANDO OPERADOR LOGICO AND
     carrito.length !== 0 && refreshTotal();
+    toastify();
     storageCarrito();
     storageCounter();
   }
